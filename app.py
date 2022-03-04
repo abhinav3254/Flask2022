37aa1085ba2226412a29aa80d2fb42ad1ff51209
abhinav3254 <abhinav3254@gmail.com>
2022-03-04 16:58:38 +0530

    Api With Enivornment SetUp in Flask

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    return 'Welcome to DashBoard'
+
+kolkata = [
+    {
+        'long':'22.0089E',
+        'lat':'80.1254N'
+    },
+    {
+        'Country':'India',
+        'state':'West Bengal',
+        'Population':'22.5 Million',
+        'Capital':'Yes'
+    }
+]
+
+@app.route('/ccu',methods=['GET'])
+def kolkata1():
+    return jsonify(kolkata)
+
+if __name__ == '__main__':
+    app.run(debug=True)
